10889c2bee7478ad9b0985625c2d57b720cfa58e
Yurii BUHAI <ybuhai@e2r14p3.unit.ua>
2019-01-25 16:59:02 +0200

    first sucsess

--- src/place.c
+++ src/place.c
@@ -6,40 +6,73 @@
 /*   By: ybuhai <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/22 14:43:34 by ybuhai            #+#    #+#             */
-/*   Updated: 2019/01/24 18:25:44 by ybuhai           ###   ########.fr       */
+/*   Updated: 2019/01/25 15:51:21 by ybuhai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
+void	clear_field(void)
+{
+	int	i;
+	char *str;
+
+	i = 0;
+	while (i < g_filler.field.height)
+		free(g_filler.field.piece[i++]);
+	free(g_filler.field.piece);
+	get_next_line(-5, &str);
+	exit (1);
+}
 
 int		place_piece(int y, int x)
 {
-	int i;
-	int j;
+	int x2;
+	int y2;
 	int count;
 
 	count = 0;
-	i = 0;
-	while (i < g_filler.piece.height)
+	y2 = 0;
+	while (y2 < g_filler.piece.height)
 	{
-		j = 0;
-		while (j < g_filler.piece.width)
+		x2 = 0;
+		while (x2 < g_filler.piece.width)
 		{
-			if (g_filler.piece.piece[i][j] == '*' && (g_filler.field.piece[y][x] ==
-									g_filler.player.number || g_filler.field.piece[y][x] == g_filler.player.number + 32))
+			if (g_filler.piece.piece[y2][x2] == '*' && (g_filler.field.piece[y2 + y][x2 + x] == g_filler.player.number || g_filler.field.piece[y2 + y][x2 + x] == g_filler.player.number + 32))
 				count++;
-			if (g_filler.piece.piece[i][j] == '*' && (g_filler.field.piece[y][x] ==
-									g_filler.enemy.number || g_filler.field.piece[y][x] == g_filler.enemy.number + 32))
+			if (g_filler.piece.piece[y2][x2] == '*' && (g_filler.field.piece[y2 + y][x2 + x] == g_filler.enemy.number || g_filler.field.piece[y2 + y][x2 + x] == g_filler.enemy.number + 32))
 				return (0);
-			j++;
-			x++;
+			x2++;
 		}
-		i++;
-		y++;
-		x -= g_filler.piece.width;
+		y2++;
 	}
 	if (count == 1)
 		return (1);
+	return (0);
+}
+
+int		up_right(void)
+{
+	int x;
+	int y;
+
+//	ft_printf("ur");
+	y = 0;
+	while (y < g_filler.field.height)
+	{
+		x = g_filler.field.width - 1;
+		while (x >= 0)
+		{
+			if (g_filler.field.height - g_filler.piece.height - y >= 0 && g_filler.field.width - g_filler.piece.width - x >= 0)
+				if (place_piece(y, x))
+				{
+					g_filler.candidat.x = x;
+					g_filler.candidat.y = y;
+					return (1);
+				}
+			x--;
+		}
+		y++;
+	}
 	return (0);
 }
 
@@ -48,81 +81,81 @@ int		down_left(void)
 	int x;
 	int y;
 
+//	ft_printf("dl");
 	y = g_filler.field.height - 1;
 	while (y >= 0)
 	{
 		x = 0;
-		while (x < g_filler.field.height)
+		while (x < g_filler.field.width)
 		{
-			if (g_filler.field.height - y - g_filler.piece.height >= 0 && g_filler.field.width - x - g_filler.piece.width >= 0)
+			if (g_filler.field.height - g_filler.piece.height - y >= 0 && g_filler.field.width - g_filler.piece.width - x >= 0)
 				if (place_piece(y, x))
 				{
-					g_filler.candidat.y = y;
 					g_filler.candidat.x = x;
+					g_filler.candidat.y = y;
 					return (1);
 				}
 			x++;
 		}
 		y--;
 	}
-	g_filler.candidat.y = 0;
-	g_filler.candidat.x = 0;
 	return (0);
 }
+
+int		up_left(void)
+{
+	int x;
+	int y;
+
+//	ft_printf("ul");
+	y = 0;
+	while (y < g_filler.field.height)
+	{
+		x = 0;
+		while (x < g_filler.field.width)
+		{
+			if (g_filler.field.height - g_filler.piece.height - y >= 0 && g_filler.field.width - g_filler.piece.width - x >= 0)
+				if (place_piece(y, x))
+				{
+					g_filler.candidat.x = x;
+					g_filler.candidat.y = y;
+					return (1);
+				}
+			x++;
+		}
+		y++;
+	}
+	return (0);
+}
+
 int		down_right(void)
 {
 	int x;
 	int y;
 
+//	ft_printf("dr");
 	y = g_filler.field.height - 1;
 	while (y >= 0)
 	{
 		x = g_filler.field.width - 1;
 		while (x >= 0)
 		{
-			if (g_filler.field.height - y - g_filler.piece.height >= 0 && g_filler.field.width - x - g_filler.piece.width >= 0)
+			if (g_filler.field.height - g_filler.piece.height - y >= 0 && g_filler.field.width - g_filler.piece.width - x >= 0)
+			{
 				if (place_piece(y, x))
 				{
-					g_filler.candidat.y = y;
 					g_filler.candidat.x = x;
+					g_filler.candidat.y = y;
 					return (1);
 				}
+			}
 			x--;
 		}
 		y--;
 	}
-	g_filler.candidat.y = 0;
-	g_filler.candidat.x = 0;
 	return (0);
 }
-int		up_right(void)
-{
-	int x;
-	int y;
-
-	y = 0;
-	while (y < g_filler.field.height)
-	{
-		x = g_filler.field.width - 1;
-		while (x >= 0)
-		{
-			if (g_filler.field.height - y - g_filler.piece.height >= 0 && g_filler.field.width - x - g_filler.piece.width >= 0)
-				if (place_piece(y, x))
-				{
-					g_filler.candidat.y = y;
-					g_filler.candidat.x = x;
-					return (1);
-				}
-			x--;
-		}
-		y++;
-	}
-	g_filler.candidat.y = 0;
-	g_filler.candidat.x = 0;
-	return (0);
-}
-
-int		up_left(void)
+void	find_player_start(void)
 {
 	int x;
 	int y;
@@ -133,27 +166,33 @@ int		up_left(void)
 		x = 0;
 		while (x < g_filler.field.width)
 		{
-			if (g_filler.field.height - y - g_filler.piece.height >= 0 && g_filler.field.width - x - g_filler.piece.width >= 0)
-				if (place_piece(y, x))
-				{
-					g_filler.candidat.y = y;
-					g_filler.candidat.x = x;
-					return (1);
-				}
+			if (g_filler.field.piece[y][x] == g_filler.player.number)
+			{
+				g_filler.player.start.x = x;
+				g_filler.player.start.y = y;
+			}
+			else if (g_filler.field.piece[y][x] == g_filler.enemy.number)
+			{
+				g_filler.enemy.start.x = x;
+				g_filler.enemy.start.y = y;
+			}
 			x++;
 		}
 		y++;
 	}
-	g_filler.candidat.y = 0;
-	g_filler.candidat.x = 0;
-	return (0);
 }
 
-void	find_current_enemy_coords(void)
+void	coords_current(void)
 {
-	int x;
-	int y;
+	int			y;
+	int			x;
+	static int	z;
 
+	if (z != 15)
+	{
+		find_player_start();
+		z = 15;
+	}
 	y = 0;
 	while (y < g_filler.field.height)
 	{
@@ -172,74 +211,18 @@ void	find_current_enemy_coords(void)
 	}
 }
 
-void	clear_field(void)
+int		make_choise(void)
 {
-	int	i;
-	char *str;
-
-	i = 0;
-	while (i < g_filler.field.height)
-		free(g_filler.field.piece[i++]);
-	free(g_filler.field.piece);
-	get_next_line(-5, &str);
-	exit (1);
-}
-
-void	coords_start(void)
-{
-	int			x;
-	int			y;
-	static int	z;
-
-	if (z != 5)
-	{
-		y = 0;
-		while (y < g_filler.field.height)
-		{
-			x = 0;
-			while (x < g_filler.field.width)
-			{
-				if (g_filler.field.piece[y][x] == g_filler.enemy.number)
-				{
-					g_filler.enemy.start.x = x;
-					g_filler.enemy.start.y = y;
-				}
-				else if (g_filler.field.piece[y][x] == g_filler.player.number)
-				{
-					g_filler.player.start.x = x;
-					g_filler.player.start.y = y;
-				}
-				x++;
-			}
-			y++;
-		}
-	}
-	z = 5;
-}
-
-int		find_place(void)
-{
-//	ft_printf("p.x-%i e.x-%i p.y-%i p.e-%i\n", g_filler.player.start.x, g_filler.enemy.start.x, g_filler.player.start.y, g_filler.enemy.start.y);
-	if (g_filler.player.start.x > g_filler.enemy.start.x && g_filler.player.start.y > g_filler.enemy.start.y)
-	{
-//		ft_printf("upleft\n");
-		return (up_left());
-	}
-	else if (g_filler.player.start.x > g_filler.enemy.start.x && g_filler.player.start.y < g_filler.enemy.start.y)
-	{
-//		ft_printf("down_left\n");
-		return (down_left());
-	}
-	else if (g_filler.player.start.x < g_filler.enemy.start.x && g_filler.player.start.y > g_filler.enemy.start.y)
-	{
-//		ft_printf("upright\n");
-		return (up_right());
-	}
-	else
-	{
-//		ft_printf("downright\n");
+//	ft_printf("my y-%i x-%i enemy y-%i x-%i\n", g_filler.player.start.y, g_filler.player.start.x, g_filler.enemy.start.y, g_filler.enemy.start.x);
+	if (g_filler.player.start.x <= g_filler.enemy.start.x && g_filler.player.start.y <= g_filler.enemy.start.y)
 		return (down_right());
-	}
+	else if (g_filler.player.start.x >= g_filler.enemy.start.x && g_filler.player.start.y <= g_filler.enemy.start.y)
+		return (down_left());
+	else if (g_filler.player.start.x >= g_filler.enemy.start.x && g_filler.player.start.y >= g_filler.enemy.start.y)
+		return (up_left());
+	else
+		return (up_right());
+	
 }
 
 void	filler(char **str)
@@ -247,13 +230,16 @@ void	filler(char **str)
 	int res;
 	int i;
 
-	g_filler.enemy.start.x = -1;
-	coords_start();
+	coords_current();
 //	find_real_piece();
-	if (g_filler.enemy.start.x == -1)
-		find_current_enemy_coords();
-//	ft_printf("+++%i\n", g_filler.enemy.start.x);
-	res = find_place();
+	res = make_choise();
+	if (!res)
+	{
+		g_filler.candidat.y = 0;
+		g_filler.candidat.x = 0;
+	}
+	g_filler.player.start.x = g_filler.candidat.x;
+	g_filler.player.start.y = g_filler.candidat.y;
 	ft_putnbr(g_filler.candidat.y);
 	ft_putchar(' ');
 	ft_putnbr(g_filler.candidat.x);
